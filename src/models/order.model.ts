13bda7ab56848cd1a43ca7dadf00085c4c8bf572
Minhajul-alam <alam.minhajul@outlook.com>
2024-07-04 16:43:07 +0600

    added priduct and order models

---
+++ src/models/order.model.ts
@@ -0,0 +1,12 @@
+import { Schema, model } from "mongoose";
+
+const orderSchema = new Schema({
+  email: { type: String, required: true },
+  productId: { type: Schema.Types.ObjectId, ref: "Product", required: true },
+  price: { type: Number, required: true },
+  quantity: { type: Number, required: true },
+});
+
+const Order = model("Order", orderSchema);
+
+export default Order;
---
+++ src/models/product.model.ts
@@ -0,0 +1,25 @@
+import { Schema, model } from "mongoose";
+
+const variantSchema = new Schema({
+  type: { type: String, required: true },
+  value: { type: String, required: true },
+});
+
+const inventorySchema = new Schema({
+  quantity: { type: Number, required: true },
+  inStock: { type: Boolean, required: true },
+});
+
+const productSchema = new Schema({
+  name: { type: String, required: true },
+  description: { type: String, required: true },
+  price: { type: Number, required: true },
+  category: { type: String, required: true },
+  tags: { type: [String], required: true },
+  variants: { type: [variantSchema], required: true },
+  inventory: { type: inventorySchema, required: true },
+});
+
+const Product = model("Product", productSchema);
+
+export default Product;
